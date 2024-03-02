350c889c3f8e2870725c8d780125c388505857f9
Trupti <patiltruptib@gmail.com>
2024-03-02 13:21:22 -0800

    Completed Upload functionality for mobile breakpoint(323px)

--- src/pages/UploadVideoPage/UploadVideoPage.jsx
+++ src/pages/UploadVideoPage/UploadVideoPage.jsx
@@ -1,10 +1,33 @@
 import "./UploadVideoPage.scss";
-const UploadVideoPage = () => {
-    return(
-    <section>
-        <h2>Upload Video</h2>
+import { Link } from "react-router-dom";
+import UploadHeroImage from "./../../assets/images/Upload-video-preview.jpg";
 
-    </section>
+/**
+ * Added UploadVideoPage component to upload videos, It has below form feilds :
+ * Video Textbox,Video Description textarea,Publish button,Cancel link
+ * @returns 
+ */
+const UploadVideoPage = () => {
+    return (
+        <section className="upload">
+            <h2 className="upload__title">Upload Video</h2>
+            <div className="upload__hero-container">
+                <div className="upload__label">VIDEO THUMBNAIL</div>
+                <div className="upload__image-container">
+                    <img className="upload__image"
+                        src={UploadHeroImage} alt="UploadVideoPreview" />
+                </div>
+                <form className="upload__form-container" id="upload-form">
+                    <label className="upload__label">TITLE YOUR VIDEO</label>
+                    <input className="upload__video-name" type="text" placeholder="Add a title to your video" />
+                    <label className="upload__label">ADD A VIDEO DESCRIPTION</label>
+                    <textarea className="upload__video-description" placeholder="Add a descrition to your video"></textarea>
+                    <button className="upload__publish-button">PUBLISH</button>
+                    <Link className="upload__cancel-link">CANCEL</Link>
+                </form>
+            </div>
+
+        </section>
     );
 }
 
