c6171de2b257da49e2f1ba452d7c56ce6ef3f0af
Trupti <patiltruptib@gmail.com>
2024-03-03 10:06:15 -0800

    Completed UploadVideo functionality for desktop breakpoint and also changed icons image(publish.svg) on Publish button

--- src/pages/UploadVideoPage/UploadVideoPage.jsx
+++ src/pages/UploadVideoPage/UploadVideoPage.jsx
@@ -11,22 +11,28 @@ const UploadVideoPage = () => {
     return (
         <section className="upload">
             <h2 className="upload__title">Upload Video</h2>
-            <div className="upload__hero-container">
-                <div className="upload__label">VIDEO THUMBNAIL</div>
-                <div className="upload__image-container">
-                    <img className="upload__image"
-                        src={UploadHeroImage} alt="UploadVideoPreview" />
+            <div className="upload__main-container">
+
+                <div className="upload__hero-container">
+                    <div className="upload__label">VIDEO THUMBNAIL</div>
+                    <div className="upload__image-container">
+                        <img className="upload__image"
+                            src={UploadHeroImage} alt="UploadVideoPreview" />
+                    </div>
                 </div>
-                <form className="upload__form-container" id="upload-form">
+            
+                <div className="upload__form-container">
                     <label className="upload__label">TITLE YOUR VIDEO</label>
                     <input className="upload__video-name" type="text" placeholder="Add a title to your video" />
                     <label className="upload__label">ADD A VIDEO DESCRIPTION</label>
                     <textarea className="upload__video-description" placeholder="Add a descrition to your video"></textarea>
-                    <div className="upload__button-container">
-                        <button className="upload__publish-button">PUBLISH</button>
-                        <Link className="upload__cancel-link">CANCEL</Link>
-                    </div>
-                </form>
+                </div>
+                
+                <div className="upload__button-container">
+                    <button className="upload__publish-button">PUBLISH</button>
+                    <Link className="upload__cancel-link">CANCEL</Link>
+                </div>
+
             </div>
         </section>
     );
